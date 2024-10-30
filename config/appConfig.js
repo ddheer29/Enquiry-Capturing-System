d8667bf84176e238cae3a36396d9836fdbb134f7
ddheer29 <divyangdheer11@gmail.com>
2024-10-30 23:55:53 +0530

    feat: Initial project setup

---
+++ config/appConfig.js
@@ -0,0 +1,7 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+export default Object.freeze({
+  PORT: process.env.PORT
+})
+
---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import appConfig from './config/appConfig.js'
+import router from './router/router.js'
+
+const { PORT } = appConfig
+
+const app = express()
+
+app.use(express.json())
+app.use('/', router)
+
+app.listen(PORT, () => {
+  console.info(`Server listening on port ${PORT}`)
+})
+
