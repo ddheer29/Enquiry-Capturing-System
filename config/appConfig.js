032a43109ebca719beef8d6cc5f84185c33c8eb4
ddheer29 <divyangdheer11@gmail.com>
2024-11-01 12:14:39 +0530

    feat: Googlesheet setup

--- config/appConfig.js
+++ config/appConfig.js
@@ -2,6 +2,8 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export default Object.freeze({
-  PORT: process.env.PORT
+  PORT: process.env.PORT,
+  GOOGLE_SHEET_CLIENT_EMAIL: process.env.GOOGLE_SHEET_CLIENT_EMAIL,
+  GOOGLE_SHEET_PRIVATE_KEY: process.env.GOOGLE_SHEET_PRIVATE_KEY
 })
 
